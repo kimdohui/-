3eff8ab0915e982f124152d69450d3317bd5a262
kimdohui <kdh24162416@gmail.com>
2020-03-14 01:30:35 +0900

    props사용하기

--- src/App.js
+++ src/App.js
@@ -2,8 +2,11 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  // return <MyComponent name="react" />;
-  return <MyComponent>리액트</MyComponent>;
+  return (
+    <MyComponent name={"react"} FavoriteNum={1}>
+      리액트
+    </MyComponent>
+  );
 };
 
 export default App;
