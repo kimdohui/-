bde574ff7263338de556ddbbd0898651a54ebe99
kimdohui <kdh24162416@gmail.com>
2020-03-14 00:30:42 +0900

    props의 간단한 활용

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React from "react";
 import MyComponent from "./MyComponent";
 
 const App = () => {
-  return <MyComponent />;
+  // return <MyComponent name="react" />;
+  return <MyComponent>리액트</MyComponent>;
 };
 
 export default App;
--- src/MyComponent.js
+++ src/MyComponent.js
@@ -1,7 +1,17 @@
 import React from "react";
 
-const MyComponent = () => {
-  return <div>나의 컴포넌트</div>;
+const MyComponent = props => {
+  return (
+    <div>
+      나의 이름은 {props.name} 이다.
+      <br />
+      children값은 {props.children}이다.
+    </div>
+  );
+};
+
+MyComponent.defaultProps = {
+  name: "기본 이름"
 };
 
 export default MyComponent;
