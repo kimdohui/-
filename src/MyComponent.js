29ff104714bf496e48c1568aa02e1a741ee58c9b
kimdohui <kdh24162416@gmail.com>
2020-03-13 23:58:29 +0900

    새 컴포넌트 생성 밑 모듈 불러오기

---
+++ src/MyComponent.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const MyComponent = () => {
+  return <div>나의 컴포넌트</div>;
+};
+
+export default MyComponent;
+/*위의 코드는 다른 파일에서 이 파일을 import할때 위에서 선언한 MyComponent 클래스를 불러오도록 설정한다. */
