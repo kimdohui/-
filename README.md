fee60548ca622bdb3ca0567f37cfd88b78828230
kimdohui <kdh24162416@gmail.com>
2020-03-15 23:15:00 +0900

    복수 input 사용하기

---
+++ README.md
@@ -0,0 +1,16 @@
+# -Vacation-assignment
+
+정보보안과 공통과제
+<br><br>
+
+# react 처음부터 시작하기
+
+react의 시작부터 활용까지 공부해나가는 repository
+<br><br><br><br>
+
+# 날짜별 공부
+
+- 2020/03/11(react 시작 밑 기본 코드 해석)
+- 2020/03/13(react 컴포넌트 생성 밑 모듈 불러오기)
+- 2020/03/14(react props와 state)
+- 2020/03/15(react 이벤트 핸들링)
--- src/EventPractice.js
+++ src/EventPractice.js
@@ -2,7 +2,29 @@ import React, { Component } from "react";
 
 class EventPractice extends Component {
   state = {
+    username: "",
     message: ""
+  };
+
+  /* 밑에 함수 선언을 화살표 함수로 선언했다면 필요 없음(메서드 바인딩 과정이 따로 필요 없음)
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+*/
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.username + " : " + this.state.message);
+    this.setState({
+      username: "",
+      message: ""
+    });
   };
 
   render() {
@@ -11,26 +33,21 @@ class EventPractice extends Component {
         <h1>이벤트test</h1>
         <input
           type="text"
+          name="username"
+          placeholder="이름"
+          value={this.state.username}
+          onChange={this.handleChange}
+        />
+
+        <input
+          type="text"
           name="message"
           placeholder="입력해주세요"
           value={this.state.message}
-          onChange={e => {
-            this.setState({
-              message: e.target.value
-            });
-          }}
+          onChange={this.handleChange}
         />
 
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: ""
-            });
-          }}
-        >
-          확인버튼
-        </button>
+        <button onClick={this.handleClick}>확인버튼</button>
       </div>
     );
   }
