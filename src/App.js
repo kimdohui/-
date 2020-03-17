ce688c9b5d132bd45e063e21d148e3e64c182d2d
kimdohui <kdh24162416@gmail.com>
2020-03-18 00:59:04 +0900

    ref를 활용한 예제(스크롤 바 위치 조정)

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import ValidationSample from "./ValidationSample";
 import React, { Component } from "react";
+import ScrollBox from "./ScrollBox";
 
 class App extends Component {
   render() {
-    return <ValidationSample />;
+    return (
+      <div>
+        <ScrollBox ref={ref => (this.ScrollBox = ref)} />
+        <button onClick={() => this.ScrollBox.scrollToBottom()}>
+          맨 아래로
+        </button>
+        <button onClick={() => this.ScrollBox.scrollToTop()}>맨 위로</button>
+      </div>
+    );
   }
 }
 
